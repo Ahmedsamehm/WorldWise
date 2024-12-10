6c0f1489482225ab5b52556a68c567478001e2b5
Ahmedsamehm <a7medxd992@hotmail.com>
2024-12-10 23:40:06 +0200

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import {
   createBrowserRouter,
   RouterProvider,
   HashRouter as Router,
+  Routes,
+  Route,
 } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
@@ -22,58 +24,73 @@ import FormCity from "./Components/FormCity";
 import { AuthProvider } from "./Contexts/FakeAuthContext";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "",
-      element: <Layout />,
-      children: [
-        {
-          index: "/",
-          element: <Home />,
-        },
-        {
-          path: "Price",
-          element: <Price />,
-        },
-        {
-          path: "Product",
-          element: <Product />,
-        },
-        {
-          path: "Login",
-          element: <Login />,
-        },
-        {
-          path: "AppLayOut",
-          element: <AppLayOut />,
-          children: [
-            {
-              path: "city",
-              element: <City />,
-            },
-            {
-              path: "Countries",
-              element: <Countries />,
-            },
-            {
-              path: "CityDetails/:id",
-              element: <CityDetails />,
-            },
-            {
-              path: "FormCity",
-              element: <FormCity />,
-            },
-          ],
-        },
-      ],
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "",
+  //     element: <Layout />,
+  //     children: [
+  //       {
+  //         index: "/",
+  //         element: <Home />,
+  //       },
+  //       {
+  //         path: "Price",
+  //         element: <Price />,
+  //       },
+  //       {
+  //         path: "Product",
+  //         element: <Product />,
+  //       },
+  //       {
+  //         path: "Login",
+  //         element: <Login />,
+  //       },
+  //       {
+  //         path: "AppLayOut",
+  //         element: <AppLayOut />,
+  //         children: [
+  //           {
+  //             path: "city",
+  //             element: <City />,
+  //           },
+  //           {
+  //             path: "Countries",
+  //             element: <Countries />,
+  //           },
+  //           {
+  //             path: "CityDetails/:id",
+  //             element: <CityDetails />,
+  //           },
+  //           {
+  //             path: "FormCity",
+  //             element: <FormCity />,
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  // ]);
 
   return (
     <>
       <AuthProvider>
         <AppContextProvider>
-          <RouterProvider router={router}></RouterProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="Price" element={<Price />} />
+                <Route path="Product" element={<Product />} />
+                <Route path="Login" element={<Login />} />
+                <Route path="AppLayOut" element={<AppLayOut />}>
+                  <Route path="city" element={<City />} />
+                  <Route path="Countries" element={<Countries />} />
+                  <Route path="CityDetails/:id" element={<CityDetails />} />
+                  <Route path="FormCity" element={<FormCity />} />
+                </Route>
+              </Route>
+            </Routes>
+          </Router>
         </AppContextProvider>
       </AuthProvider>
     </>
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/WorldWise/", // Repository name here
   plugins: [react()],
-  base:"/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
